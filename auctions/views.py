3f6c679a7b61fc89d5f7ede41388945782daa69d
Lucas Pulsz <lucaspulsz@gmail.com>
2020-10-01 00:38:43 -0300

    db interaction

--- auctions/views.py
+++ auctions/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 import datetime
+
 from .models import User, Listing, Bids, Comments
 
 
@@ -88,7 +89,43 @@ def register(request):
 
 def create_listing(request):
     if request.method == "POST":
-        return HttpResponse("BROTA PAI! BROTA!")
+        # get posted content
+        nulo = ""
+        listing = Listing()
+        
+        listing.title = request.POST["title"]
+        listing.price = request.POST["price"]
+        listing.description = request.POST["description"]
+        listing.category = request.POST["category"]
+        listing.image = request.POST["image"]
+        listing.creator_id = request.user.id
+        listing.time = datetime.datetime.now()
+
+        if listing.image == nulo:
+            listing.image = "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-1.jpg"
+        
+        if listing.title == nulo:
+            return render(request, "auctions/create_listing.html", {
+                "listings": Listing.objects.all(),
+                "message": "Please, provide valid information",
+                "class": "alert alert-danger"
+            })
+        try:
+            # save into db
+            listing.save()
+
+        except ValueError:
+            return render(request, "auctions/create_listing.html", {
+                "listings": Listing.objects.all(),
+                "message": "Please, provide valid information",
+                "class": "alert alert-danger"
+            })
+
+        return render(request, "auctions/index.html", {
+            "listings": Listing.objects.all(),
+            "message": "Listing created succesfully",
+            "class": "alert alert-success"
+        })
 
     else:
         username = request.user.username
