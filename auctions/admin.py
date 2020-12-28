68f0e1301efe061f2fe8e0b4bfea867e9791a77c
Lucas Pulsz <lucaspulsz@gmail.com>
2020-12-28 18:36:41 -0300

    the end

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Listing, Bids, Comments
+from .models import User, Listing, Bids, Comments, Watchlist, Closed
 
 # Register your models here.
     #adding db into django admin interface
@@ -7,3 +7,5 @@ admin.site.register(User)
 admin.site.register(Listing)
 admin.site.register(Bids)
 admin.site.register(Comments)
+admin.site.register(Watchlist)
+admin.site.register(Closed)
